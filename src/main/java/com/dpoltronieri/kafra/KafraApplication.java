9da143bc46b1de40bc320cff58bacd0708093159
Poltronieri <danppoltronieri@gmail.com>
2024-12-27 20:08:44 +0000

    fist commit

---
+++ src/main/java/com/dpoltronieri/kafra/KafraApplication.java
@@ -0,0 +1,13 @@
+package com.dpoltronieri.kafra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpoltronieri/kafra/KafraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpoltronieri.kafra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
